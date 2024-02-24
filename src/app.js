b493e1a5481027ba937bb2ca77152f7e073fe728
Mujahid789 <mujahidinfo789@gmail.com>
2024-02-24 16:30:20 +0500

    utill settings to handle errors

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express, { request } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+
+}));
+// express.json()
+app.use(express.json({limit:"16kb"}))
+// urlencoded is to take data from url
+app.use(express.urlencoded({extended:true, limit:"16kb"}))
+// express. static is used to store static files
+app.use(express.static("public"))
+// cookieParser is used to get and set data in user's browser cookies 
+app.use(cookieParser());
+
+app.get('/user', (req, res) => {
+    res.send(`i am mujahid`);
+    // res.send(`user ${req.params.id}`)
+  })
+
+
+export {app}
---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+// import dotenv from "dotenv";
+
+// dotenv.config()
+
+const connectDB = async ()=>{
+    try {
+        console.log(process.env.MONGODB_URI);
+        console.log(DB_NAME)
+        const connectionDB = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+                
+        console.log(`\n db connected successfully DB Host ${connectionDB.connection.host}`);
+        
+    } catch (error) {
+        console.log("mongoDB connection error", error);
+        process.exit(1);
+    }
+}
+export default connectDB;
