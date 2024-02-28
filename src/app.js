9d9249a3973e6db310f76eb3624aa38daf8d9041
Mujahid789 <mujahidinfo789@gmail.com>
2024-02-28 12:35:51 +0500

    router and controller all setup configured

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express, { request } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+
 const app = express();
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -17,13 +18,11 @@ app.use(express.static("public"))
 // cookieParser is used to get and set data in user's browser cookies 
 app.use(cookieParser());
 
-app.get('/user', (req, res) => {
-    res.send(`i am mujahid`);
-    // res.send(`user ${req.params.id}`)
-  })
-  app.use('/user/profile/:userName', (req,res)=>{
-    res.send(`user profile is ${req.params.userName}`);
-})
+
+// routes import 
+import userRouter from "./routes/user.routes.js";
+
+app.use('/api/v1/users', userRouter)
 
 
 export {app}
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -38,7 +38,7 @@ const userSchema = new Schema({
             ref: "Video"
         }
     ],
-    passwoed:{
+    password:{
         type: String,
         required: [true, "password is required"]
     },
@@ -51,17 +51,17 @@ const userSchema = new Schema({
 
 userSchema.pre("save" , async function(next){
     if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10)
+    this.password =await bcrypt.hash(this.password, 10)
         next();
-    
+     
 })
 
-userSchema.methods.isPasswordCorrect = async function(passwoed){
-   return await bcrypt.compare(passwoed, this.password)
+userSchema.methods.isPasswordCorrect = async function(password){
+   return await bcrypt.compare(password, this.password)
 }
 
 userSchema.methods.generateAccessToken = function(){
-   return jwt.sign({ ///// payload
+   return Jwt.sign({ ///// payload
         _id: this.id,
         email: this.email,
         userName : this.userName,
@@ -75,7 +75,7 @@ userSchema.methods.generateAccessToken = function(){
     )
 }
 userSchema.methods.generateRefreshToken = function(){
-    return jwt.sign({ ///// payload
+    return Jwt.sign({ ///// payload
         _id: this.id,
         
 
